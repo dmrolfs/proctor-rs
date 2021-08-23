f2f726a01cd5956df70ca3ea6f12b80c27d1a43b
Damon Rolfs <drolfs@gmail.com>
2021-08-23 10:29:10 -0700

    make TelemetrySource public

--- src/phases/collection/mod.rs
+++ src/phases/collection/mod.rs
@@ -1,6 +1,6 @@
 pub use clearinghouse::*;
 pub use settings::*;
-pub use source::{make_telemetry_cvs_source, make_telemetry_rest_api_source};
+pub use source::{make_telemetry_cvs_source, make_telemetry_rest_api_source, TelemetrySource};
 pub use subscription_channel::SubscriptionChannel;
 
 mod clearinghouse;
